a605014e08d816c03b93442c551bac18e905d8f4
hnaufal-telkom <hnaufal@student.telkomuniversity.ac.id>
2025-03-09 20:14:32 +0700

    menambahkann implementasi dengan table-driven

--- tpmodul4_103022300163/Program.cs
+++ tpmodul4_103022300163/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		KodePos kodePos = new KodePos();
+		Console.WriteLine(kodePos.getKodePos("Batununggal").ToString());
+	}
+}
+
+class KodePos
+{
+	public int getKodePos(string kelurahan)
+	{
+		switch (kelurahan)
+		{
+			case "Batununggal":
+				return 40266;
+			case "Kujangsari":
+				return 40287;
+			case "Mengger":
+				return 40267;
+			case "Wates":
+				return 40256;
+			case "Cijaura":
+				return 40287;
+			case "Jatisari":
+				return 40286;
+			case "Margasari":
+				return 40286;
+			case "Sekejati":
+				return 40286;
+			case "Kebonwaru":
+				return 40272;
+			case "Maleer":
+				return 40274;
+			case "Samoja":
+				return 40273;
+			default:
+				return 0;
+		}
+	}
+}
--- tpmodul4_103022300163/obj/Release/net8.0/tpmodul4_103022300163.AssemblyInfo.cs
+++ tpmodul4_103022300163/obj/Release/net8.0/tpmodul4_103022300163.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103022300163")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d8f2e0ddceecf473e4a6879f6b07a5268cde95")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103022300163")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103022300163")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
